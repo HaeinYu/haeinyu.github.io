09040eec2a452a70a2dd29dbe8cce38842db743f
Haein Yu <jessyyu5656@gmail.com>
2023-10-07 12:12:35 -0400

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // header--dark style is applied to the Header when scrolling down
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
     if(window.scrollY > headerHeight) {
@@ -9,3 +9,10 @@ document.addEventListener('scroll', () => {
         header.classList.remove('header--dark');
     }
 })
+
+// Make Home background transparent when scroll down
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
